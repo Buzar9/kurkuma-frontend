193c0688dc5723948800f8939eb0608085501338
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-06-26 17:31:39 +0200

    Rename

--- src/pages/achievements-list/achievements-list.component.jsx
+++ src/pages/achievements-list/achievements-list.component.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCodeBranch, faCrown, faFireAlt, faHourglassHalf, faList, faStarOfLife} from "@fortawesome/free-solid-svg-icons";
 
-export default class AchievementsList extends Component {
+export default class AchievementsListComponent extends Component {
 
     constructor(props) {
         super(props);
